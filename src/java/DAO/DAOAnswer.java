ec260ef759c5310b9763c0f70fc99e2ba4ceb0e9
tuanpmhe161621 <tuanpmhe161621@fpt.edu.vn>
2023-06-08 16:53:24 +0700

    Update CourseController, DAOQuestion, DAOAnswer, create jspClient/Learning.jsp, Entities Question, Answer

--- src/java/DAO/DAOAnswer.java
+++ src/java/DAO/DAOAnswer.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+import Entities.Answer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 import module.DBConnect;
 /**
  *
  * @author admin
  */
 public class DAOAnswer extends DBConnect{
+    
+    public Vector<Answer> getAll(String sql) {
+        Vector<Answer> vector = new Vector<Answer>();
+        ResultSet rs = this.getData(sql);
+        try {
+            while (rs.next()) {
+                int answer_id = rs.getInt(1);
+                String answer_name = rs.getString(2);
+                int is_correct = rs.getInt(3);
+                int question_id = rs.getInt(4);
+                Answer answer = new Answer(question_id, answer_name, is_correct, question_id);
+                vector.add(answer);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return vector;
+    }
     
 }
--- src/java/DAO/DAOQuestion.java
+++ src/java/DAO/DAOQuestion.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+import Entities.Question;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 import module.DBConnect;
 
 /**
@@ -10,5 +14,22 @@ import module.DBConnect;
  * @author admin
  */
 public class DAOQuestion extends DBConnect{
+    
+    public Vector<Question> getAll(String sql) {
+        Vector<Question> vector = new Vector<Question>();
+        ResultSet rs = this.getData(sql);
+        try {
+            while (rs.next()) {
+                int question_id = rs.getInt(1);
+                String question_name = rs.getString(2);
+                int course_id = rs.getInt(3);
+                Question question = new Question(question_id, question_name, course_id);
+                vector.add(question);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return vector;
+    }
     
 }
